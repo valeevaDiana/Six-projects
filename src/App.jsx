c52990884fc3e6ada93f9f5e4e96e762412d5cfa
hjgdfkudnxhfd <valeev@rtural.ru>
2025-09-14 21:25:43 +0500

    add underline to menu

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,13 @@ import ModalWindow from "./Apps/ModalWindow";
 
 function App() {
   const [currentApp, setCurrentApp] = useState('1');
+  const [activeItem, setActiveItem] = useState('1');
+
+  const handleItemClick = (appName) => {
+    setCurrentApp(appName);
+    setActiveItem(appName);
+  };
+
 
   const renderApp = () => {
     switch (currentApp) {
@@ -23,12 +30,12 @@ function App() {
     <div>
       <header>
         <ul className='navigation'>
-          <li onClick={() => setCurrentApp('1')}>Counter</li>
-          <li onClick={() => setCurrentApp('2')}>Modal Window</li>
-          <li onClick={() => setCurrentApp('3')}>app3</li>
-          <li onClick={() => setCurrentApp('4')}>app4</li>
-          <li onClick={() => setCurrentApp('5')}>app5</li>
-          <li onClick={() => setCurrentApp('6')}>app6</li>
+          <li className={activeItem === '1' ? 'active' : ''} onClick={() => handleItemClick('1')}>Counter</li>
+          <li className={activeItem === '2' ? 'active' : ''} onClick={() => handleItemClick('2')}>Modal Window</li>
+          <li className={activeItem === '3' ? 'active' : ''} onClick={() => handleItemClick('3')}>app3</li>
+          <li className={activeItem === '4' ? 'active' : ''} onClick={() => handleItemClick('4')}>app4</li>
+          <li className={activeItem === '5' ? 'active' : ''} onClick={() => handleItemClick('5')}>app5</li>
+          <li className={activeItem === '6' ? 'active' : ''} onClick={() => handleItemClick('6')}>app6</li>
         </ul>
       </header>
       <main>{renderApp()} </main>
